02101a0cd4efb5c73d91478e3c5a1df21c0a0c01
Pedro Lamas <pedrolamas@gmail.com>
2019-06-28 23:31:58 +0100

    Updates of all packages to latest available

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -26,49 +26,6 @@ declare module '@mdx-js/react' {
   const MDXProvider: React.FunctionComponent<MDXProviderProps>;
 }
 
-declare module 'disqus-react' {
-  import * as React from "react";
-
-  interface DisqusProps {
-    shortname: string;
-    config: {
-      url: string;
-      identifier: string;
-      title: string;
-    };
-  }
-
-  interface CommentCountProps extends DisqusProps {
-    children?: React.ReactNode;
-  }
-
-  interface CommentEmbedProps {
-    commentId: string;
-    showParentComment?: boolean;
-    showMedia?: boolean;
-    width?: number;
-    height?: number;
-  }
-
-  interface IDisqus {
-    CommentCount: React.Component<CommentCountProps, {}>;
-    CommentEmbed: React.Component<CommentEmbedProps, {}>;
-    DiscussionEmbed: React.Component<DisqusProps, {}>;
-  }
-
-  class CommentCount extends React.Component<CommentCountProps, {}> {}
-  class CommentEmbed extends React.Component<CommentEmbedProps, {}> {}
-  class DiscussionEmbed extends React.Component<DisqusProps, {}> {}
-  const Disqus: {
-    CommentCount: React.ComponentType<CommentCountProps>;
-    CommentEmbed: React.ComponentType<CommentEmbedProps>;
-    DiscussionEmbed: React.ComponentType<DisqusProps>;
-  };
-
-  export { CommentCount, CommentEmbed, DiscussionEmbed };
-  export default Disqus;
-}
-
 declare namespace NodeJS {
   interface ProcessEnv {
     readonly NODE_ENV: 'development' | 'production' | 'test';
